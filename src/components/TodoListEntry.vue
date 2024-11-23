2bd81529a2d34377991cf1cd38d160400eeef5fd
Colin <crrood@gmail.com>
2024-11-23 10:58:34 -0800

    Change TodoListEntry to grid layout

--- src/components/TodoListEntry.vue
+++ src/components/TodoListEntry.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="flex place-items-center space-x-6 m-4 p-4" 
+  <div class="grid grid-cols-8 space-x-4 m-4 p-4 text-white border-2 border-white" 
       @click="router.push({ 
       name: 'Todo',
       params: { id: todo._id.$oid, todo: JSON.stringify(todo) }})">
-      <div class="text-white">
-        <div class="text-bold">TodoListEntry:</div>
-        {{ todo }}
-      </div>
+    <div class="col-span-2">{{ todo.name }}</div>
+    <div class="text-center">{{ todo.status }}</div>
+    <div class="text-center">{{ todo.urgency }}</div>
+    <div class="text-center">{{ todo.impact }}</div>
+    <div class="text-center">{{ todo.effort }}</div>
+    <div class="col-span-2">{{ todo.notes }}</div>
   </div>
 </template>
 
