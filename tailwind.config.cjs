2ea068e3f9dfa043b5b7ef69e68c9233fa0bfc7d
Colin <crrood@gmail.com>
2024-11-28 14:00:54 -0800

    Add secondary-dark, update btn-primary

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#000000',
-        'secondary': '#299446',
+        'primary': '#0f172a', // slate-900
+        'secondary': '#15803d', // green-800
+        'secondary-dark': '#166534', // green-900
         'accent': '#6B21A8', // purple-800
         'accent-dark': '#581C87',  // purple-900
       }
