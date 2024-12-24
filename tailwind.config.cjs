95764c17cd0654ce7cbd5866e4475024ae9152f0
Colin <crrood@gmail.com>
2024-12-24 11:05:01 -0800

    Change secondary color to slate-600

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,8 @@ module.exports = {
       colors: {
         'primary': '#0f172a', // slate-900
         'primary-light': '#1e293b', // slate-800
-        'secondary': '#15803d', // green-800
-        'secondary-dark': '#166534', // green-900
+        'secondary': '#475569', // slate-600
+        'secondary-dark': '#334155', // slate-700
         'accent': '#6B21A8', // purple-800
         'accent-dark': '#581C87',  // purple-900
       }
